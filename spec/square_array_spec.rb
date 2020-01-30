9c15558122a0630d01852955be99a43a191bf7c8
Beth Ruff <beth.ruff2@gmail.com>
2020-01-30 20:22:39 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -6,3 +6,11 @@ def square_array(array)
   square_array("numbers")
 end
 
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
+
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
