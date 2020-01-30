4b8a25d489f9834296316137564101a0a2962920
Beth Ruff <beth.ruff2@gmail.com>
2020-01-30 20:19:35 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,8 +1,7 @@
-  def square_array(array)
-    numbers = [1,2,3]
-    array.each do |n|
-        numbers = n ** 2
-    end
-    square_array
+def square_array(array)
+  numbers = [1, 2, 3]
+  numbers.each do |n|
+    numbers = (n ** 2)
   end
+end
 
