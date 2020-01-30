0862ed8fe17928ce384caafdee23da6c821114ef
Beth Ruff <beth.ruff2@gmail.com>
2020-01-30 19:42:53 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,12 +1,7 @@
-describe '#square_array' do
-
-
     numbers = [1,2,3]
-    expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
-    expect(numbers).to_not receive(:inject)
+    
     square_array(numbers)
-  end
+  
 
   it 'calls on each' do
     numbers = [1,2,3]
