4956dd66f95610eb5c0589eb37c4861987153c65
Beth Ruff <beth.ruff2@gmail.com>
2020-01-30 19:57:47 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -5,12 +5,3 @@
     
     square_array
     
-    
-  
-
-  it 'should square the elements in an array' do
-    expect(square_array([1,2,3])).to eq([1,4,9])
-    expect(square_array([9,10,16,25])).to eq([81,100,256,625])
-  end
-
-end
