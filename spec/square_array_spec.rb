eb1990b47735a9ec91e1c026a8667cc4db92440c
Beth Ruff <beth.ruff2@gmail.com>
2020-01-30 19:35:21 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -13,8 +13,6 @@ describe '#square_array' do
     numbers.each do |number|
       puts "#{number}**"
   end
-  
-  numbers
 
   it 'should square the elements in an array' do
     expect(square_array([1,2,3])).to eq([1,4,9])
